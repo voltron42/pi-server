2f0ab6673bec4ccfb9ce097897f2d85575ab4681
voltron42 <dajohnson1978@gmail.com>
2020-07-27 18:17:13 +0000

    updating clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.flatland/ordered "1.5.7"]
                  [clj-commons/secretary "1.2.4"]
                  [http-kit "2.3.0"]
